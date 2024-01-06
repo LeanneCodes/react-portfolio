971ed5177402782c09b867321eb973821a894646
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-06 14:44:25 +0000

    amend color scheme and font for home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,12 +5,12 @@ import '../styles/Home.css'
 const Home = () => {
   return (
     <div className='intro-container container-fluid'>
-      <h1 style={{color: 'white'}}>Leanne Goldsmith</h1>
+      <h1 style={{fontFamily: 'var(--title)', fontSize: '7rem'}}>Leanne<br />Goldsmith</h1>
       <p style={{color: 'white'}}>
-      I hold a Merit Diploma as a certified Fullstack Engineer, with a profound passion for Frontend development.
+      Certified Fullstack Engineer with a Merit Diploma, passionate about Frontend development.
       </p>
       <div className='intro-links'>
-        <button type='button'><a href='#' target='_blank'>Preview CV {<RxOpenInNewWindow />}</a></button>
+        <button type='button'><a href='#' target='_blank'>Resume {<RxOpenInNewWindow />}</a></button>
       </div>
     </div>
   )
