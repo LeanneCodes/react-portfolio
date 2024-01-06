a27cad077520f846a5053dfd9fb9795b4827fc51
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-06 22:34:32 +0000

    start project section

---
+++ src/components/elements/Card.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../styles/Card.css';
+
+const Card = (props) => {
+  return (
+    <div className='project-card'>
+      <div className='project-image'>
+        <img src={props.img} alt="project-image" />
+      </div>
+      <div className='project-content'>
+        <div className='project-info'>
+          <h4>{props.name}</h4>
+          <h5>{props.tech}</h5>
+          <p className='w-75 mx-auto'>{props.summary}</p>
+          <button>{props.link}</button>
+          <div className='project-link'>
+            <a href={props.link1} target="_blank">View Project</a>
+            <a href={props.link2} target="_blank">View GitHub</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import '../styles/Projects.css';
+import Card from '../elements/Card';
+import Oyster from "../../assets/oyster-and-beyond-project-cover.png"
+import VaultKey from '../../assets/vaultkey-project-cover.png';
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='projects-container container-fluid' id="projects">
+      <h1>My Projects</h1>
+      <div className='projects'>
+        <div className='projects-card'>
+          <Card
+            img={ Oyster }
+            name="Oyster & Beyond Blog"
+            tech="HTML, NextJs, Tailwind, JavaScript, Hygraph CMS"
+            summary="A travel blog that showcases a variety of destinations and an option for users to leave comments."
+            link1="https://oyster-and-beyond-blog.vercel.app/"
+            link2="https://github.com/LeanneCodes/oyster-and-beyond-blog"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
