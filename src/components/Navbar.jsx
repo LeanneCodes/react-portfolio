1e24b3242a4bface512b289cad6d2bcdac8e9c46
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-06 13:01:15 +0000

    change navbar to vertical position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,88 +4,82 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container-fluid">
-        <div className='title'>
+    <>
+      
+      <ul className="nav flex-column">
+      <div className='title'>
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Portfolio
+        </NavLink>
+      </div>
+        <li className="nav-item">
           <NavLink
-            to="/"
-            end
+          to="/"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="about"
             className={({ isActive }) =>
               isActive ? 'nav-link active' : 'nav-link'
             }
           >
-            Junior Frontend Portfolio
+            About Me
           </NavLink>
-        </div>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse justify-content-end pe-md-4" id="navbarText">
-          <ul className="navbar-nav mb-2 mb-lg-0">
-          <li className="nav-item">
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="about"
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-              >
-                About Me
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="skills"
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-              >
-                Skills
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="projects"
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-              >
-                Projects
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="contact"
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-              >
-                Contact Me
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="resume"
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-              >
-                Resume
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="skills"
+            className={({ isActive }) =>
+              isActive ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Skills
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="projects"
+            className={({ isActive }) =>
+              isActive ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Projects
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="contact"
+            className={({ isActive }) =>
+              isActive ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Contact Me
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="resume"
+            className={({ isActive }) =>
+              isActive ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Resume
+          </NavLink>
+        </li>
+      </ul>
+    </>
   )
 }
 
