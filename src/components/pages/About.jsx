9125f4be5bd634babcbc3389c43ddeda57acdaa8
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-07 19:55:46 +0000

    home responsive

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -54,7 +54,7 @@ const About = () => {
           During my career, I specialised in Addressable Advertising, building display ad banners for clients such as, Eurostar, Audible and Legoland. I discovered that I love transforming Figma and Photoshop designs into live ads using HTML, CSS, JavaScript and Greensock.
           <br/>
           <br/>
-          To have a thorough understanding of Frontend Development, I completed two bootcamps. <a href='https://www.credential.net/abe70729-0419-4049-a752-62b032d231eb#gs.08qmqt' target='_blank' style={{color: 'var(--coral)'}}>Fullstack Merit Diploma</a> and <a href='#' target='_blank' style={{color: 'var(--coral)'}}>Frontend Development Certificate</a>.
+          To have a thorough understanding of Frontend Development, I completed two bootcamps. <a href='https://www.credential.net/abe70729-0419-4049-a752-62b032d231eb#gs.08qmqt' target='_blank' style={{color: 'var(--coral)'}}>Fullstack Merit Diploma</a> and Frontend Development Certificate pending.
           <br/>
           <br/>
           Now, I'm eager to transition into a full-time Frontend Developer role, collaborating with dedicated professionals and continuously learning and growing in this exciting tech world. I'm ready to bring my skills to innovative projects and stay ahead in this ever-evolving industry.
