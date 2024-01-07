febf14ead6bc81ad3845a02c24d63acad9dbb323
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-01-07 18:11:55 +0000

    move images to src folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -93,7 +93,7 @@ const Navbar = () => {
                 </NavLink>
               </li>
               <li className="nav-item">
-                <a className='nav-link' href='/Leanne-Goldsmith-Resume.pdf' target='_blank' rel="noopener noreferrer">Resume</a>
+                <a className='nav-link' href='/src/assets/Leanne-Goldsmith-Resume.pdf' target='_blank' rel="noopener noreferrer">Resume</a>
               </li>
               <hr />
               <div className='ext-link'>
@@ -165,7 +165,7 @@ const Navbar = () => {
           </NavLink>
         </li>
         <li className="nav-item">
-          <a className='nav-link resume' href='/Leanne-Goldsmith-Resume.pdf' target='_blank' rel="noopener noreferrer">Resume</a>
+          <a className='nav-link resume' href='/src/assets/Leanne-Goldsmith-Resume.pdf' target='_blank' rel="noopener noreferrer">Resume</a>
         </li>
         <hr />
         <div className='ext-link'>
